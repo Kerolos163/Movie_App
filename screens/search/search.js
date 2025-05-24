65f982c076ca0b4a1ecbc50b5189638a3cea1f00
kerolos <kokoessa951@gmail.com>
2025-05-25 01:56:02 +0300

    feat: implement search functionality with data fetching and filtering in Search component

--- screens/search/search.js
+++ screens/search/search.js
@@ -1,11 +1,34 @@
-import React from "react";
 import { Pressable, TextInput, View } from "react-native";
 import styles from "./style";
 import { XMarkIcon } from "react-native-heroicons/outline";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import constant from "../../utils/constant";
 
 const Search = () => {
+  const [allData, setallData] = useState([]);
+  const [SelectedData, setSelectedData] = useState([]);
   const navigation = useNavigation();
+  useEffect(() => {
+    axios
+      .get(`${constant.baseUrl}/discover/movie?api_key=${constant.apiKey}`)
+      .then((res) => {
+        console.log(res.data.results);
+        setallData([...res.data.results]);
+      });
+  }, []);
+
+  const filterData = (event) => {
+    let value = event.nativeEvent.text;
+    // console.log(allData);
+    setSelectedData(
+      allData.filter((item) => {
+        return item.title.toLowerCase().includes(value.toLowerCase());
+      })
+    );
+    console.log(SelectedData);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.SearchContainer}>
@@ -14,13 +37,12 @@ const Search = () => {
           style={styles.search}
           selectionColor={"gray"}
           placeholderTextColor={"orange"}
+          onChange={(e) => filterData(e)}
         ></TextInput>
         <Pressable style={styles.btn} onPress={() => navigation.goBack()}>
           <XMarkIcon size={30} color={"gray"}></XMarkIcon>
         </Pressable>
       </View>
-
-      
     </View>
   );
 };
