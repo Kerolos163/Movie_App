6fc901cfdc238c08d56fd6c198792f9d36857e51
kerolos <kokoessa951@gmail.com>
2025-05-24 21:16:06 +0300

    feat: enhance FavoriteItem component with navigation and update MoviePage to log parameters

--- components/favorite/favoriteItem.js
+++ components/favorite/favoriteItem.js
@@ -1,33 +1,46 @@
 import { HeartIcon } from "react-native-heroicons/solid";
 import { StyleSheet, View, Image, Text, Pressable } from "react-native";
 import constant from "../../utils/constant";
+import { useNavigation } from "@react-navigation/native";
+import router from "../../utils/router";
 
 const FavoriteItem = ({ data }) => {
+  const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      <Pressable
-        style={({ pressed }) => [
-          styles.IconStyle,
-          { opacity: pressed ? 0.5 : 1 },
-        ]}
-        onPress={() => console.log("Wesso")}
-      >
-        <HeartIcon size={36} color={"orange"}></HeartIcon>
-      </Pressable>
-      <Image
-        style={styles.imageStyle}
-        source={{ uri: `${constant.imageUrl}/${data.item.poster_path}` }}
-      ></Image>
-      <View style={styles.blurStyle}></View>
-      <Text style={styles.title}>{data.item.title}</Text>
-    </View>
+    <Pressable
+      onPress={() => navigation.navigate(router.Movie, data.item)}
+      style={({ pressed }) => [
+        styles.Pressable,
+        { opacity: pressed ? 0.9 : 1 },
+      ]}
+    >
+      <View style={styles.container}>
+        <Pressable
+          style={({ pressed }) => [
+            styles.IconStyle,
+            { opacity: pressed ? 0.5 : 1 },
+          ]}
+          onPress={() => console.log("Wesso")}
+        >
+          <HeartIcon size={36} color={"orange"}></HeartIcon>
+        </Pressable>
+        <Image
+          style={styles.imageStyle}
+          source={{ uri: `${constant.imageUrl}/${data.item.poster_path}` }}
+        ></Image>
+        <View style={styles.blurStyle}></View>
+        <Text style={styles.title}>{data.item.title}</Text>
+      </View>
+    </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  Pressable: {
     width: "49.3%",
     aspectRatio: 3 / 4,
+  },
+  container: {
     borderRadius: 10,
     position: "relative",
   },
